226205716c3cba51f9fdc3848cabd56cfd2d4ea1
Đăng Khôii <dangkhoii@MacBook-Pro-cua-ang.local>
2025-04-09 09:59:39 +0700

    da chinh frontend

--- frontend/src/layouts/MainLayouts.tsx
+++ frontend/src/layouts/MainLayouts.tsx
@@ -11,7 +11,7 @@ interface MainLayoutProps {
 
 const MainLayout: React.FC<MainLayoutProps> = ({ children }) => {
   return (
-    <div className="main-background">
+    <div className="main-background"> 
       <div className="main-layout">
         <Header />
         <div className="main-content">
