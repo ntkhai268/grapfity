afdf0dcd139191aabc25d961b0ca6efe94b89443
Đăng Khôii <dangkhoii@192.168.1.3.non-exists.ptr.local>
2025-04-12 14:17:10 +0700

    da xong trang chu

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -6,34 +6,42 @@ import Login from "./container/Login";
 import Homepage from "./container/HomePage";
 
 import LoginLayout from "./layouts/LoginLayouts";
-import MainLayout from "./layouts/MainLayouts";
+import HomeLayout from "./layouts/HomeLayouts";
 
 import { PlayerProvider } from "./context/PlayerContext";
 
 const App: React.FC = () => {
+  // return (
+  //   <PlayerProvider>
+  //     <Routes>
+  //       <Route
+  //         path="/"
+  //         element={
+  //           <LoginLayout>
+  //             <Login />
+  //           </LoginLayout>
+  //         }
+  //       />
+  //       <Route
+  //         path="/mainpage"
+  //         element={
+  //           <MainLayout>
+  //             <Homepage />
+  //           </MainLayout>
+  //         }
+  //       />
+  //       <Route path="*" element={<Navigate to="/" replace />} />
+  //     </Routes>
+  //   </PlayerProvider>
+  // );
   return (
     <PlayerProvider>
-      <Routes>
-        <Route
-          path="/"
-          element={
-            <LoginLayout>
-              <Login />
-            </LoginLayout>
-          }
-        />
-        <Route
-          path="/mainpage"
-          element={
-            <MainLayout>
-              <Homepage />
-            </MainLayout>
-          }
-        />
-        <Route path="*" element={<Navigate to="/" replace />} />
-      </Routes>
+      <HomeLayout>
+        <Homepage />
+      </HomeLayout>
     </PlayerProvider>
   );
+  
 };
 
 export default App;
