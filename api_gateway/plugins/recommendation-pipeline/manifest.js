c038ab4a03c89c485cbaba87b4ae48d812082141
Thanh Khai Nguyen <khainguyen3626@gmail.com>
2025-06-03 21:11:40 +0700

    merge cua hoang thanh cong

--- api_gateway/plugins/recommendation-pipeline/manifest.js
+++ api_gateway/plugins/recommendation-pipeline/manifest.js
@@ -4,13 +4,13 @@ module.exports = {
   version: '0.1.0',
   init: (pluginContext) => {
     // const policy = require('./policies/recommendation-pipeline-policy'); 
-    const policy = require('./policies/index'); 
+    const policy = require('./policies/index');
     pluginContext.registerPolicy(policy);
   },
   policies: ['recommendation-pipeline-policy'],
   schema: {
-  "$id": "http://express-gateway.io/schemas/plugins/recommendation-pipeline.json",
-}
+    "$id": "http://express-gateway.io/schemas/plugins/recommendation-pipeline.json",
+  }
 
 };
 
